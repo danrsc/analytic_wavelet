7679c16bc81748b6513bb781ec065fe2af28d891
Piergiacomo De Marchi <piergiacomo.demarchi@gmail.com>
2020-04-16 14:09:18 +0200

    Fixing log_spaced_frequencies() function to support case when low=None (which was always skipped). Also substituted parameter in the call to _low_frequency_cutoff(). endpoint_overlap (not r) is the Python variable corresponding to the Matlab variable low{1} used in https://github.com/jonathanlilly/jLab/blob/0ddd2b11b6511152dfaca8764fe202c8acd7f1d9/jWavelet/morsespace.m

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='analytic_wavelet',
+      version='0.1',
+      description='A translation of J.M. Lilly\'s code for ridge and element analysis using generalized Morse wavelets into python',
+      url='https://github.com/danrsc/analytic_wavelet',
+      author='danrsc',
+      author_email='',
+      license='MIT',
+      packages=['analytic_wavelet'],
+      zip_safe=False)
